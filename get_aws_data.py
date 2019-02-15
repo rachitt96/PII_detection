c5e29c8cd65618f172de3bbebfdb43d1a7584916
rachitt96 <rachitt96@gmail.com>
2019-02-15 18:52:38 -0400

    Commiting datasets and python programs

---
+++ get_aws_data.py
@@ -0,0 +1,17 @@
+#This program will download the test data (stored in S3 bucket) to "data" folder. 
+
+import boto3
+
+s3_resource = boto3.resource('s3')
+s3_client = boto3.client('s3')
+
+try:
+	s3_resource.Bucket('enronsamples').download_file('test_emails.tsv', 'data/test_emails.tsv')
+	print('Successfully downloaded the test dataset')
+
+except Exception as exc:
+	print(exc)
+	
+
+
+
