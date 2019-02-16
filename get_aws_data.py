90aac0370aaf31804d8eb1b01ac5c77db7384b31
rachitt96 <rachitt96@gmail.com>
2019-02-15 20:10:24 -0400

    Removed unncessory comments

--- get_aws_data.py
+++ get_aws_data.py
@@ -1,19 +1,14 @@
+#This program will download the test data (stored in S3 bucket) to "data" folder. 
+
 import boto3
 
 s3_resource = boto3.resource('s3')
 s3_client = boto3.client('s3')
 
-'''
-for bucket in s3.buckets.all():
-	print(bucket.name)
-'''
-'''
-bucket_policies = s3_client.get_bucket_acl(Bucket = 'enronsamples')
-print(bucket_policies)
-'''
-
 try:
-	s3_resource.Bucket('enronsamples').download_file('test_emails.tsv', 'aws_downloaded.tsv')
+	s3_resource.Bucket('enronsamples').download_file('test_emails.tsv', 'data/aws_downloaded.tsv')
+	print('Successfully downloaded the test dataset')
+
 except Exception as exc:
 	print(exc)
 	
